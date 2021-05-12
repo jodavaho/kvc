e4aac76529313dddcbaab40065e03be38ba31658
Josh Vander Hook <hook@jpl.nasa.gov>
2021-05-12 16:45:58 -0700

    1.0

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ use std::io::Lines;
 /// assert_eq!(kvc::version(),"0.5.5");
 /// ```
 pub fn version() -> String{
-    return "0.5.6".to_string();
+    return "1.0.0".to_string();
 }
 
 /// Get the reserved keyword matchers as a HashMap<String,regex::Regex> 
